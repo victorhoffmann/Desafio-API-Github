92063103c09410b1bd409f77ce89c71eb1f1b468
viictorhoffmann <viictorhoffmann@gmail.com>
2021-02-11 17:29:09 -0300

    bugfix: corrigindo datas

--- index.js
+++ index.js
@@ -6,28 +6,25 @@ document.querySelector('button').onclick = () => {
     const retorno = (result) => {
         for (const campo in result){
             if (document.querySelector('#'+campo)){
-
-                if ((document.querySelector('#'+campo)) === (document.querySelector('#updated_at'))) {
-                    let data = result[campo]
-                    let split = data.split('T');
-                    let formatada = split[0].split('-');
-                    let final = formatada[2]+'/'+formatada[1]+'/'+formatada[0];
-                    document.querySelector('#updated_at').innerText = 'Última atualização: ' + final;
-                } 
-
-                if ((document.querySelector('#'+campo)) === (document.querySelector('#created_at'))) {
-                    let data = result[campo]
-                    let split = data.split('T');
-                    let formatada = split[0].split('-');
-                    let final = formatada[2]+'/'+formatada[1]+'/'+formatada[0];
-                    document.querySelector('#created_at').innerText = 'Desde: ' + final;
-                } 
-                
-                else {
-                    document.querySelector('#'+campo).innerText = result[campo];
-                    document.querySelector('#'+campo).src = result[campo];
-                }
+                document.querySelector('#'+campo).innerText = result[campo];
+                document.querySelector('#'+campo).src = result[campo];
             }
+    
+            if ((document.querySelector('#'+campo)) === (document.querySelector('#updated_at'))) {
+                let data = result[campo]
+                let split = data.split('T');
+                let formatada = split[0].split('-');
+                let final = formatada[2]+'/'+formatada[1]+'/'+formatada[0];
+                document.querySelector('#updated_at').innerText = 'Última atualização: ' + final;
+            } 
+
+            if ((document.querySelector('#'+campo)) === (document.querySelector('#created_at'))) {
+                let data = result[campo]
+                let split = data.split('T');
+                let formatada = split[0].split('-');
+                let final = formatada[2]+'/'+formatada[1]+'/'+formatada[0];
+                document.querySelector('#created_at').innerText = 'Desde: ' + final;
+            } 
         }
     }  
     document.querySelector('#avatarBlank').href = `https://github.com/${usuario.value}`;
