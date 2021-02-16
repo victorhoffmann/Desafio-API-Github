0cce93a36aeeb053a1fbc143b219ad2f4dffa5b1
viictorhoffmann <viictorhoffmann@gmail.com>
2021-02-16 12:13:38 -0300

    feature: simplificando formatData

--- index.js
+++ index.js
@@ -19,20 +19,11 @@ document.querySelector('button').onclick = () => {
             }
     
             if ((document.querySelector('#'+campo)) === (document.querySelector('#updated_at'))) {
-                let data = formataData(result[campo])
-                document.querySelector('#updated_at').innerText = 'Última atualização: ' + data;
+                document.querySelector('#updated_at').innerText = 'Última atualização: ' + new Date(result[campo]).toLocaleDateString();
             } 
 
             if ((document.querySelector('#'+campo)) === (document.querySelector('#created_at'))) {
-                let data = formataData(result[campo])
-                document.querySelector('#created_at').innerText = 'Desde: ' + data;
-            } 
-
-            function formataData(dates) {
-                let split = dates.split('T');
-                let formatada = split[0].split('-');
-                let final = formatada[2]+'/'+formatada[1]+'/'+formatada[0];
-                return final
+                document.querySelector('#created_at').innerText = 'Desde: ' + new Date(result[campo]).toLocaleDateString();
             }
         }
         document.querySelector('#avatarBlank').href = `https://github.com/${usuario.value}`;
